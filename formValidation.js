a86df82c383b6b7772f2cc0f7598dae0c47fba30
santiparra <122759810+Santiparra@users.noreply.github.com>
2023-02-05 19:29:34 -0300

    o

---
+++ formValidation.js
@@ -0,0 +1,8 @@
+const userMail = document.getElementById("email");
+const userCountry = document.getElementById("country");
+const userZipC = document.getElementById("zip-code");
+const userpsw = document.getElementById("password");
+const pswconfirm = document.getElementById("confirm-password")
+
+/* clases wrongInput  goodInput */
+
